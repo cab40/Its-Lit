d29220f87d1d70a270bf5105a0de8706a70b53bb
Raymond Wu <raymond.w.78.in@gmail.com>
2021-09-19 05:14:38 +0100

    move things around, fix a bit

--- embed/main.c
+++ embed/main.c
@@ -23,10 +23,10 @@
 
 //wiringPI pin config;
 #define BG_DATA_PIN 0 //output pins for the 8 bit shift register - parallel out
-#define BG_LATCH_PIN 2 //for blue and green leds
+#define BG_LATCH_PIN 2
 #define BG_CLOCK_PIN 3 
 
-#define RY_DATA_PIN 1 //for red and yellow leds
+#define RY_DATA_PIN 1
 #define RY_LATCH_PIN 4
 #define RY_CLOCK_PIN 5
 
@@ -35,9 +35,69 @@
 #define RED_BUTTON 28
 #define YELLOW_BUTTON 29 
 
-#define shiftRemove3(a) ((a >> 1) & 0xF7)
+//this macro shifts then removes the 4th bit.. 0x77 is 0b01110111
+//purpose: each row of 4 lights is in half of a byte, 1 bit each after each time interval, 
+//we need to move lights down so shift each 4 to the left but when top 4 are shifted, it bleeds into bottom 4
+//so set the top bit of the bottom half to 0... we will set left most bit in top half to 0 too for safety.
+#define shiftRemove3(a) ((a >> 1) & 0x77)
 
-void initialize(int * GPIOPINS, int * GPIODIR, int n){ //GPIODIR -> array of integers, 0 for output, 1 for input
+void initialize(int * GPIOPINS, int * GPIODIR, int n); //GPIODIR -> array of integers, 0 for output, 1 for input
+void destroyPins(int * GPIOPINS, int n);
+void sendData(u_int8_t dataPin, u_int8_t clockPin, u_int8_t latchPin, u_int8_t data);
+
+int main(){
+    u_int8_t pins[] = {BG_DATA_PIN, BG_LATCH_PIN, BG_CLOCK_PIN, RY_DATA_PIN, RY_LATCH_PIN, RY_CLOCK_PIN, BLUE_BUTTON, GREEN_BUTTON, RED_BUTTON, YELLOW_BUTTON};
+    int pinModes[] = {0,0,0,0,0,0,1,1,1,1};
+
+    u_int8_t blueGreen = 0;
+    u_int8_t redYellow = 0;
+    int musicNotes[] = {4, 3, 2, 1, 0, 15, 12, 9, 7, 0, -1};
+    int musicNotesTest[] = {15,0,0,0,0,15,0,13,12,0,0,0,0,0,0,1,0,0,0,0,0,-1};
+    int musicNotesTest4[] = {8,0,0,0,0,-1};
+    int musicNotesTest3[] = {3,0,0,0,0,3,0,3,3,0,0,0,0,0,0,2,0,0,0,0,0,-1};
+    int musicNotesTest2[] = {3,0,0,0,0,-1};
+    //Next note is in binary
+    //lowest 4 bits bbbb denote if there is a next note for red, yellow, green, blue respectively
+    //so 1000 or 8 means there is a note for yellow next, -1 is end
+
+    wiringPiSetup();
+    for(int i = 0;i < 10;i++){
+        if(pinModes[i]){
+            pinMode(pins[i], INPUT);
+        } else {
+            pinMode(pins[i], OUTPUT);
+        }
+    }
+
+    int i = 0;
+    int shiftBits[2]; //(blue, green) and (yellow, red)
+    while(musicNotesTest[i] != -1){
+        shiftBits[0] = musicNotesTest[i] & 1;
+        shiftBits[1] = (musicNotesTest[i] & (1 << 1)) >> 1;
+        blueGreen = shiftRemove3(blueGreen)| (shiftBits[1] << 7 | shiftBits[0] << 3);
+
+        shiftBits[0] = (musicNotesTest[i] & (1 << 2)) >> 2;
+        shiftBits[1] = (musicNotesTest[i] & (1 << 3)) >> 3;
+        redYellow = shiftRemove3(redYellow)| (shiftBits[1] << 7 | shiftBits[0] << 3);
+
+        sendData(BG_DATA_PIN, BG_CLOCK_PIN, BG_LATCH_PIN, blueGreen);
+        sendData(RY_DATA_PIN, RY_CLOCK_PIN, RY_LATCH_PIN, redYellow); 
+        i++;
+        delay(500);
+    }
+
+    for(int j = 0;j < 4;j++){ //shift 4 more times at end of song to make sure the notes finish
+        blueGreen = shiftRemove3(blueGreen);
+        redYellow = shiftRemove3(redYellow);
+        sendData(BG_DATA_PIN, BG_CLOCK_PIN, BG_LATCH_PIN, blueGreen);
+        sendData(RY_DATA_PIN, RY_CLOCK_PIN, RY_LATCH_PIN, redYellow);
+        delay(500);
+    }
+
+    exit(0);
+}
+
+void initialize(int * GPIOPINS, int * GPIODIR, int n){
     int exportFD = open("/sys/class/gpio/export", O_WRONLY);
     for(int i = 0;i < n;i++){
         write(exportFD, GPIOPINS[i], 2);
@@ -51,7 +111,6 @@ void initialize(int * GPIOPINS, int * GPIODIR, int n){ //GPIODIR -> array of int
 }
 
 //to set a value to pins, you can open /sys/class/gpio/gpioNUMBEr/value and write to it either 1 or 0
-
 void destroyPins(int * GPIOPINS, int n){
     int unexportFD = open("/sys/class/gpio/unexport", O_WRONLY);
     for(int i = 0;i < n;i++){
@@ -79,57 +138,4 @@ void sendData(u_int8_t dataPin, u_int8_t clockPin, u_int8_t latchPin, u_int8_t d
   digitalWrite(clockPin, LOW);
   digitalWrite(dataPin, LOW);
   digitalWrite(latchPin, HIGH);
-}
-
-int main(){
-    u_int8_t pins[] = {BG_DATA_PIN, BG_LATCH_PIN, BG_CLOCK_PIN, RY_DATA_PIN, RY_LATCH_PIN, RY_CLOCK_PIN, BLUE_BUTTON, GREEN_BUTTON, RED_BUTTON, YELLOW_BUTTON};
-    int pinModes[] = {0,0,0,0,0,0,1,1,1,1};
-
-    u_int8_t blueGreen = 0;
-    u_int8_t redYellow = 0;
-    int musicNotes[] = {4, 3, 2, 1, 0, 15, 12, 9, 7, 0, -1};
-    int musicNotesTest[] = {15,0,0,0,0,15,0,13,12,0,0,0,0,0,0,1,0,0,0,0,0,-1};
-    int musicNotesTest4[] = {8,0,0,0,0,-1};
-    int musicNotesTest3[] = {3,0,0,0,0,3,0,3,3,0,0,0,0,0,0,2,0,0,0,0,0,-1};
-    int musicNotesTest2[] = {3,0,0,0,0,-1};
-    //Next note is in binary
-    //lowest 4 bits bbbb denote if there is a next note for red, yellow, green, blue respectively
-    //so 1000 or 8 means there is a note for yellow next, -1 is end
-
-    wiringPiSetup();
-    for(int i = 0;i < 10;i++){
-        if(pinModes[i]){
-            pinMode(pins[i], INPUT);
-        } else {
-            pinMode(pins[i], OUTPUT);
-        }
-    }
-
-    int i = 0;
-    int shiftBits[2]; //(blue, green) and (red, yellow)
-    while(musicNotesTest[i] != -1){
-        shiftBits[0] = musicNotesTest[i] & 1;
-        shiftBits[1] = (musicNotesTest[i] & (1 << 1)) >> 1;
-        blueGreen = shiftRemove3(blueGreen)| (shiftBits[1] << 7 | shiftBits[0] << 3);
-
-        shiftBits[0] = (musicNotesTest[i] & (1 << 2)) >> 2;
-        shiftBits[1] = (musicNotesTest[i] & (1 << 3)) >> 3;
-        redYellow = shiftRemove3(redYellow)| (shiftBits[1] << 7 | shiftBits[0] << 3);
-
-        sendData(BG_DATA_PIN, BG_CLOCK_PIN, BG_LATCH_PIN, blueGreen);
-        sendData(RY_DATA_PIN, RY_CLOCK_PIN, RY_LATCH_PIN, redYellow); 
-        i++;
-        delay(500);
-    }
-
-    for(int j = 0;j < 4;j++){ //finish the sequence here
-        blueGreen = shiftRemove3(blueGreen);
-        redYellow = shiftRemove3(redYellow);
-        sendData(BG_DATA_PIN, BG_CLOCK_PIN, BG_LATCH_PIN, blueGreen);
-        sendData(RY_DATA_PIN, RY_CLOCK_PIN, RY_LATCH_PIN, redYellow);
-        delay(500);
-    }
-
-    exit(0);
-    //return 0;
 }
