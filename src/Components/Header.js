7987aad0ab2ddd53dfa0aa96b3204542cd0b54cf
Catherine Burns <catherineburns8@gmail.com>
2021-09-19 08:31:30 -0400

    text

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
     render(){
         return (
             <div className = 'navbar-style'>
-                <h2 href="#home" className="title-big glow">PIANO TILES</h2>
+                <h2 href="#home" className="title-big glow">IT'S LIT</h2>
             </div>
         )
     }
