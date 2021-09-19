f46d3f676b1c73cec29585a906ad61f5eabdefee
Alvin Dai <alvin.dai.02@gmail.com>
2021-09-19 00:26:56 -0400

    carousel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Header from './Components/Header.js';
 import Cards from './Components/Cards.js';
 import {data} from './Components/data.js'
+import SongCarousel from './Components/SongCarousel.js';
 
 
 class App extends React.Component{
@@ -18,8 +19,10 @@ class App extends React.Component{
     return (
       <div className="App" style={{width:'100%', height:'100%'}}>
         <Header />
+        <div style={{height: "50px"}}/>
         <div className = 'row'>
-          <Cards songs={this.state.songs}/>
+          {/*<Cards songs={this.state.songs}/>*/}
+          <SongCarousel style={{height: "50%"}}/>
         </div>
       </div>
     )
---
+++ src/Components/SongCarousel.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Carousel from "react-spring-3d-carousel";
+import uuidv4 from "uuid";
+import { config } from "react-spring";
+import SongCard from "./SongCard";
+
+class SongCarousel extends Component {
+  state = {
+    goToSlide: 0,
+    offsetRadius: 1,
+    showNavigation: false,
+    config: config.gentle
+  };
+
+  slides = [
+    {
+      key: uuidv4(),
+      content: <SongCard height = {"100%"} img_url="https://miro.medium.com/max/681/1*EBOL4lka5QjcYoxj6AHp-g.png" alt="1" title="Conversations" artist="Juice Wrld" stars={3}/>
+    },
+    {
+      key: uuidv4(),
+      content: <SongCard height={"100%"} img_url="https://images.squarespace-cdn.com/content/v1/59e68533d7bdce9ebbc3cef8/1508314396682-AGNY197TJ8159B3L3BD7/graduation.jpg" alt="2" title="Stronger" artist="Kanye West" stars={2}/>
+    },
+    {
+      key: uuidv4(),
+      content: <SongCard height={"100%"} img_url="https://i.mdel.net/i/db/2019/12/1255378/1255378-800w.jpg" alt="3" title="Golden" artist="Harry Styls" stars={3}/>
+    }
+  ].map((slide, index) => {
+    return { ...slide, onClick: () => this.setState({ goToSlide: index }) };
+  });
+
+  render() {
+    return (
+      <div style={{ width: "80%", height: "500px", margin: "0 auto" }}>
+        <Carousel
+          style={{height: "20%", width: "100%"}}
+          slides={this.slides}
+          goToSlide={this.state.goToSlide}
+          offsetRadius={this.state.offsetRadius}
+          showNavigation={this.state.showNavigation}
+          animationConfig={this.state.config}
+        />
+       </div>
+    ) 
+  }
+}
+
+export default SongCarousel;
